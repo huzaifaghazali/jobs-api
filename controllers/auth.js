c42489b1aed9c7b2537fb7dc755b8021a29959c1
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-05 20:47:14 +0500

    Add error check in auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,13 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
 
 const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  // throw error if one of the values is missing
+  if (!name || !email || !password) {
+    throw new BadRequestError('Please provide name, email and password');
+  }
   const user = await User.create({ ...req.body });
   res.status(StatusCodes.CREATED).json({ user });
 };
