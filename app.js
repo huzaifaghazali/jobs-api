c2e20977d2356fde3ed21b7c7eabffa0fa7f7e38
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-05 19:41:12 +0500

    Set up the routes

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// connectDb
+
+// routers
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -12,9 +18,8 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
