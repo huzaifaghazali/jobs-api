59d85aff8f20f9c196dbf3d81bfee2c35e4f3726
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-05 21:03:26 +0500

    Add method to hash password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,23 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError } = require('../errors');
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
+  
   // throw error if one of the values is missing
   if (!name || !email || !password) {
     throw new BadRequestError('Please provide name, email and password');
   }
-  const user = await User.create({ ...req.body });
+
+  const salt = await bcrypt.genSalt(10); // generate random bytes
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const tempUser = {name, email, password:hashedPassword};
+
+  const user = await User.create({ ...tempUser });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
