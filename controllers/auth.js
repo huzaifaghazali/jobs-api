1f19af1e2fb717933449c57dadfdda946b71c31b
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-06 19:34:23 +0500

    Set up the login controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const User = require('../models/User');
 
 const { StatusCodes } = require('http-status-codes');
-const { BadRequestError } = require('../errors');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 const register = async (req, res) => {
   // create a user
@@ -9,12 +9,29 @@ const register = async (req, res) => {
 
   // Create token
   const token = user.createJWT();
-  
+
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
-  res.send('login user');
+  const { email, password } = req.body;
+
+  // Throws error if one of the values are empty
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password');
+  }
+
+  const user = await User.findOne({ email });
+
+  // If user does not exist
+  if (!user) {
+    throw new UnauthenticatedError('Invalid Credentials');
+  }
+
+  // Create token
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
