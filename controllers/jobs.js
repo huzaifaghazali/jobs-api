f8f5bf91001753d7acb867da66912a2ae7418285
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-06 20:41:21 +0500

    Test auth middleware

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -5,7 +5,7 @@ const getJob = async (req, res) => {
    res.send('Get single job');
 }
 const createJob = async (req, res) => {
-   res.send('create job');
+   res.json(req.user);
 }
 const updateJob = async (req, res) => {
    res.send('update job');
