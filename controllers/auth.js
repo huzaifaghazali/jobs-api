2a56d17e6ef26d51727c3c20f2d9057d6648aba8
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-06 18:32:28 +0500

    Add mongoose middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,19 +5,8 @@ const { StatusCodes } = require('http-status-codes');
 const { BadRequestError } = require('../errors');
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
-  
-  // throw error if one of the values is missing
-  if (!name || !email || !password) {
-    throw new BadRequestError('Please provide name, email and password');
-  }
 
-  const salt = await bcrypt.genSalt(10); // generate random bytes
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  const tempUser = {name, email, password:hashedPassword};
-
-  const user = await User.create({ ...tempUser });
+  const user = await User.create({ ...req.body });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -22,5 +23,12 @@ const UserSchema = new mongoose.Schema({
     minlength: 6,
   },
 });
+
+// Mongoose Middleware
+// Hash the password
+UserSchema.pre('save', async function() {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+})
 
 module.exports = mongoose.model('User', UserSchema);
