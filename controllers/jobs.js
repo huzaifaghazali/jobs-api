07e2b0917a8df711f983985c62ab3935268072aa
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-07 19:24:58 +0500

    Get single job

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -7,17 +7,35 @@ const getAllJobs = async (req, res) => {
   const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt');
   res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
+
 const getJob = async (req, res) => {
-  res.send('Get single job');
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId,
+  });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${job}`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
 };
+
 const createJob = async (req, res) => {
   req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
+
 const updateJob = async (req, res) => {
   res.send('update job');
 };
+
 const deleteJob = async (req, res) => {
   res.send('delete job');
 };
