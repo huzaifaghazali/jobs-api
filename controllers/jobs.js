e116603248fd731ce1239daeb55d4b4d61efab16
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-07 19:41:10 +0500

    Update job

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -27,14 +27,37 @@ const getJob = async (req, res) => {
 };
 
 const createJob = async (req, res) => {
-   // assign the user id
+  // assign the user id
   req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = async (req, res) => {
-  res.send('update job');
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  if (company === '' || position === '') {
+    throw new BadRequestError('Company or Position fields cannot be empty');
+  }
+
+  const job = await Job.findByIdAndUpdate(
+    {
+      _id: jobId,
+      createdBy: userId,
+    },
+    req.body,
+    { new: true, runValidators: true }
+  );
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const deleteJob = async (req, res) => {
