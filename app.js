718c01edea654f319e3ab9182a534b3de143143b
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-09 23:12:05 +0500

    Add secruity setup

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
 
+// security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 
@@ -17,12 +23,21 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 const authenticateUser = require('./middleware/authentication');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs',authenticateUser, jobsRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -31,7 +46,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
